594ae3298536cb7d366f304a78a8a68acd08534a
ShulmanK <shulman.k.y@gmail.com>
2018-12-23 22:41:17 +0200

    Updates

---
+++ precache-manifest.f4d1b6a517bfa347f63c908327fa8253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "f65e42a23304fb27e3c7",
+    "url": "/contactmanager/static/js/main.f65e42a2.chunk.js"
+  },
+  {
+    "revision": "2ad202a2090bcaf2ba79",
+    "url": "/contactmanager/static/js/1.2ad202a2.chunk.js"
+  },
+  {
+    "revision": "f65e42a23304fb27e3c7",
+    "url": "/contactmanager/static/css/main.6bf063a1.chunk.css"
+  },
+  {
+    "revision": "2ad202a2090bcaf2ba79",
+    "url": "/contactmanager/static/css/1.c7872992.chunk.css"
+  },
+  {
+    "revision": "e7a09b181b76c8bfb8e2d3de0be4d043",
+    "url": "/contactmanager/index.html"
+  }
+];
